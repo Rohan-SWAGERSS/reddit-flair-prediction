b9f3def78b5a5ddf86c7eb5530f7b7a56a15e43d
Rohan Rai <97662930+Rohan-SWAGERSS@users.noreply.github.com>
2023-06-24 20:35:17 +0530

    first commit
    includes all the main files except HTML, CSS, and javascript templates. along with data scraping, data cleaning, and model creation.

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, make_response
+import predictor
+from predictor import predict, FlairActual
+import json
+import os
+from werkzeug.utils import secure_filename
+
+port = int(os.environ.get("PORT",5000))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def detect():
+    redditURL = request.form['redditpost']
+    print(redditURL)
+    predicted_flair = str(predict(str(redditURL)))
+    actualflair = str(FlairActual(str(redditURL)))
+    print(predicted_flair, actualflair)
+    return render_template('index.html', predicted_flair = predicted_flair, actualflair = actualflair)
+
+
+@app.route("/automated_testing")
+def index():                                                                                         
+    return (render_template("testing.html"))
+
+@app.route("/automated_testing", methods = ['POST', 'GET'])
+def automated_testing():
+    if request.method == 'POST':
+        myfile = request.files['upload_file']
+        myfile.save(secure_filename(myfile.filename))
+        lst = []
+        with open(myfile.filename, 'r') as filein:
+            for url in filein:
+                lst.append(url)
+        result = {}
+        for i in lst:
+            i = i[:-1]
+            pred = predict(i)
+            key = i
+            value = pred
+            result.update({key : value})
+    d = json.dumps(result)
+    return json.dumps(d)
+
+@app.errorhandler(404)
+@app.errorhandler(500)
+def errors(error):
+    return (render_template('notfound.html'))
+
+if __name__ == '__main__':
+    app.run()
---
+++ predictor.py
@@ -0,0 +1,55 @@
+import re
+import nltk
+import joblib
+import pickle
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+import praw
+import pandas as pd
+import numpy as np
+import sklearn
+
+model = pickle.load(open('models/final_model_askscience.pkl','rb'))
+reddit = praw.Reddit(client_id="JRcFlWEyHaacXWOTDcxsHw",
+                     client_secret="2wTFbG3TSqZoSVc8wlr1S_tICeIUYA",
+                     user_agent="scrapper 1.0 by /u/rohan_swagers")
+
+REPLACE_BY_SPACE = re.compile('[/(){}\[\]\|@,;]')
+REPLACE_SYMBOL = re.compile('[^0-9a-z #+_]')
+STOPWORDS = set(stopwords.words('english'))
+
+
+# Let's define the function for cleaning
+
+def clean_data(text):
+    text = BeautifulSoup(text, 'xml').text
+    text = text.lower()
+    text = REPLACE_BY_SPACE.sub(' ', text)
+    text = REPLACE_SYMBOL.sub('', text)
+    text = ' '.join(word for word in text.split() if word not in STOPWORDS)
+    return text
+
+
+# Define string function
+
+def stringConvert(text):
+    return str(text)
+
+
+# Define predict function
+
+def predict(url):
+    posts = {}
+    submission = reddit.submission(url=url)
+    posts['title'] = str(submission.title)
+    posts['body'] = str(submission.selftext)
+    posts['url'] = str(submission.url)
+    posts['title'] = clean_data(str(posts['title']))
+    posts['body'] = clean_data(str(posts['body']))
+    combine = posts['title'] + posts['body'] + posts['url']
+    return (model.predict([combine])[0])
+
+def FlairActual(url):
+    submission = reddit.submission(url=url)
+    actualflair = submission.link_flair_text
+    return actualflair
